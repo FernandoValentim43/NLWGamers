5d0f315e309bd12df7bcb1a420c570039cfe1dc0
fernando <fernandoreal.valentim@gmail.com>
2022-09-26 22:18:53 -0300

    Initial config

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -1,10 +1,33 @@
-import express from "express";
+import express, { request } from "express";
 
 
 const app = express()
 
-app.get('/ads', (request,response) => {
-   return response.json([1,2,3,4,5]);
-});
+app.get('/games', (request, response) => {
+   return response.json([]);
+})
 
+
+app.post('/ads', (request, response) => {
+   return response.status(201).json([]);
+})
+
+
+app.get('/games/:id/ads', (request, response) => {
+   
+
+   return response.json([
+      { id: 1, name: 1},
+      { id: 2, name: 2},
+      { id: 3, name: 3},
+      { id: 4, name: 4},
+      { id: 5, name: 5},
+      { id: 6, name: 6},
+   ]);
+})
+
+
+app.get('/ads/:id/discord', (request, response) => {
+   return response.json([]);
+})
 app.listen(3333);
