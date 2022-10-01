9afebcf9231d31f4efdd0d832d60f35cd0b7ee9c
fernando <fernandoreal.valentim@gmail.com>
2022-10-01 16:40:47 -0300

    .

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -2,11 +2,22 @@ import express, { request } from "express";
 import { PrismaClient } from "@prisma/client";
 
 const app = express()
-const prisma  = new PrismaClient();
+const prisma  = new PrismaClient({
+   log: ['query']
+});
 
 app.get('/games', async(request, response) => {
-   const games = await prisma.game.findMany()
-   return response.json([]);
+      const games = await prisma.game.findMany({
+         include: {
+            _count: {
+               select: {
+                  ads: true
+               }
+            }
+         }
+      })
+
+   return response.json([games]);
 })
 
 
